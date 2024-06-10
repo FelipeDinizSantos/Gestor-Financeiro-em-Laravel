8ec6f7cd8866c6b71408733d06121c636ddd9108
Felipe Diniz <felipedinizdossantos0@gmail.com>
2024-06-10 10:53:23 -0300

    Fix System Boot Error
    Fix Laravel initialization error, resulting from the lack of the sessions table, necessary for the framework to work

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// <start> Cadastro de gastos recorrentes
+
+Route::post('/', function(){
+    dd('Hello, World!');
+});
+
+// <end> Cadastro de gastos reccorentes
